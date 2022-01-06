44113139e00518c181ad4094619a5e786803c8c4
k-kinoshita <kinoken5939@gmail.com>
2022-01-06 17:56:07 +0900

    ファイル作り直し

---
+++ Rakuhan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rakuhan
+//
+//  Created by 木下健一 on 2022/01/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
