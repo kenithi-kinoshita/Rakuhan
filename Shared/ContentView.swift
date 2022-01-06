9203cc46babcc45c0a69737968cfd9fcd853e6d7
k-kinoshita <kinoken5939@gmail.com>
2022-01-06 17:49:21 +0900

    first commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 木下健一 on 2022/01/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/RakuhanApp.swift
@@ -0,0 +1,17 @@
+//
+//  RakuhanApp.swift
+//  Shared
+//
+//  Created by 木下健一 on 2022/01/06.
+//
+
+import SwiftUI
+
+@main
+struct RakuhanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
